858cef4d33de623f9051dd9ada3ce9988f379c7c
Isuru <isuruj@zone24x7.com>
2015-03-29 14:39:07 +0530

    Added Chrome packaging support.

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,21 @@ gulp.task('browserify', function(){
 gulp.task('copy', function(){
     gulp.src('src/index.html')
     .pipe(gulp.dest('dist'));
+	
+	gulp.src('src/manifest.json')
+    .pipe(gulp.dest('dist'));
+	
+	gulp.src('src/js/background.js')
+    .pipe(gulp.dest('dist/js'));
 
     gulp.src('src/css/style.css')
     .pipe(gulp.dest('dist/css'));
+	
+	gulp.src('src/assets/icon_016.png')
+    .pipe(gulp.dest('dist/assets'));
+	
+	gulp.src('src/assets/icon_128.png')
+    .pipe(gulp.dest('dist/assets'));
 });
 
 gulp.task('default', ['browserify', 'copy']);
