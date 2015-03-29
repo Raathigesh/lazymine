f33553a5a2096671d5fa111c566556db5a84a8c0
Isuru <isuruj@zone24x7.com>
2015-03-29 23:08:30 +0530

    Added frameless UI

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-	// Uncomment below to make it frameless
-	//'frame': 'none', 
+	'frame': 'none', 
     'bounds': {
       'width': screen.width / 2,
       'height': screen.height /2
