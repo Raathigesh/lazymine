088cc21ea271617391dbd9427ad72f6df756a03f
zoneisuru <isuruj@zone24x7.com>
2015-04-30 13:48:45 +0530

    Handling mouse events on the search.

--- src/js/components/app-SearchItem.js
+++ src/js/components/app-SearchItem.js
@@ -1,6 +1,7 @@
 /*Modification of http://tonyspiro.com/dev/react-typeahead-search*/
 /** @jsx React.DOM */
 var React = require('react');
+var AppActions = require('../actions/app-actions');
 
 var SearchItem = React.createClass({
 
@@ -16,6 +17,25 @@ var SearchItem = React.createClass({
       });
     },
 
+    mouseOver: function () {		
+		this.props.clearCurrent();
+        this.setState( {
+            "Classes": "result active"
+        });
+    },
+
+    click: function () {
+        var id = $('#search-results .result.active').attr('data-id');
+        AppActions.addIssue(id);
+        this.props.togglePanel(false);
+    },
+
+    mouseOut: function () {
+        this.setState( {
+            "Classes": "result"
+        });
+    },
+
     getInitialState: function () {
       return {
        "Classes": "result"
@@ -29,7 +49,7 @@ var SearchItem = React.createClass({
       
       return (
         <li>
-          <div className={this.state.Classes} id={"result-" + id} data-id={id}>
+          <div className={this.state.Classes} id={"result-" + id} data-id={id} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} onClick={this.click}>
             <div className="searchResult-projectName" dangerouslySetInnerHTML={{__html: item.project.name}}></div>
             <span className="searchResult-description" dangerouslySetInnerHTML={{__html: item.formattedTitle}}></span>
           </div>
--- src/js/components/app-SearchResult.js
+++ src/js/components/app-SearchResult.js
@@ -6,31 +6,50 @@ var SearchResult = React.createClass({
 
 	ActiveItem : 0,
 
-    getCurrentActiveResult: function(){
+    getCurrentActiveResult: function(){		
         return this.refs["searchItem" + this.ActiveItem];
     },
 
     getNextResult: function(){
         this.ActiveItem += 1;
-        return this.refs["searchItem" + this.ActiveItem];
+		if(this.refs["searchItem" + this.ActiveItem]){
+			return this.refs["searchItem" + this.ActiveItem];
+		}
+		else {
+			this.ActiveItem -= 1;
+			return null;
+		}
     },
 
     getPreviousResult: function(){
-        this.ActiveItem -= 1;
-        return this.refs["searchItem" + this.ActiveItem];
+		if(this.ActiveItem > 0){
+			this.ActiveItem -= 1;
+			return this.refs["searchItem" + this.ActiveItem];
+		}
+		else {
+			return null;
+		}
     },
+	
+	clearCurrentActiveResult: function(){
+		var currentActiveResult = this.getCurrentActiveResult();
+		
+		if(currentActiveResult){
+		  currentActiveResult.removeActive();
+		}		
+	},
 
-    moveUp: function(){
-        var currentActiveResult = this.getCurrentActiveResult();
-        var previousResult = this.getPreviousResult();
+    moveUp: function(){		
+		var currentActiveResult = this.getCurrentActiveResult();
+		var previousResult = this.getPreviousResult();
 
-        if(currentActiveResult){
-          currentActiveResult.removeActive();
-        }
+		if(currentActiveResult){
+		  currentActiveResult.removeActive();
+		}
 
-        if(previousResult){
-          previousResult.addActive();
-        }
+		if(previousResult){
+		  previousResult.addActive();
+		}		
     },
 
     moveDown: function(){
@@ -43,39 +62,48 @@ var SearchResult = React.createClass({
 
         if(nextResult){
           nextResult.addActive();
-      }
+		}
     },
 
     // Handle navigating through the result set using arraw keys
-    navigate: function (event) {
-        var nextActiveItem = null;
+    navigate: function (event) {        
         switch (event.which) {
         case 38: // up
             this.moveUp();
+			event.preventDefault();
             break;
         case 40: // down
             this.moveDown();
+			event.preventDefault();
             break;
         case 13: // enter        
             var id = $('#search-results .result.active').attr('data-id');
             AppActions.addIssue(id);            
             this.props.toggleResultsPanel(false);
             break;
+		case 27: // esc
+			this.props.toggleResultsPanel(false);
         default:
             return; // exit this handler for other keys
         }
     },
 
+    togglePanel: function(show){
+        this.props.toggleResultsPanel(show);
+    },
+
     // React's magical render method 
     render: function() {
         var rows,
-            items = this.props.results;
+            items = this.props.results,
+            toggle = this.togglePanel,
+			clear = this.clearCurrentActiveResult;
 
         if(items){
           rows = items.map(function(item, i) {
             var searchItemRef = "searchItem" + i;
             return(
-              <SearchResultItem item={item} id={item.id} ref={searchItemRef}/>
+              <SearchResultItem item={item} id={item.id} togglePanel={toggle} clearCurrent={clear} ref={searchItemRef}/>
             );
           });
         }
