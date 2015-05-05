e62c9c7d4634bccf3a902bbdb7d7437838bb2577
zoneisuru <isuruj@zone24x7.com>
2015-05-05 23:26:26 +0530

    Updated gulp build process. It takes more time now :D

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,77 @@
-var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
-var NwBuilder = require('node-webkit-builder');
-var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var imagemin = require('gulp-imagemin');
 var karma = require('karma').server;
+var minifyCss = require('gulp-minify-css');
+var nwBuilder = require('node-webkit-builder');
+var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
 
+var bases = {
+ src: 'src/',
+ dist: 'dist/',
+ concat: 'dist/concat',
+ webkit: 'dist/build/'
+};
+
+var paths = {
+	main: ['js/main.js'], // since we need to browserify this file specifically
+	scripts: ['js/shell/*.js'],
+	libs: ['js/lib/*.*', 'css/lib/*.*', 'fonts/*.*'],
+	styles: ['css/*.*'],
+	html: ['index.html'],
+	images: ['assets/*.png'],
+	extras: ['package.json'],
+};
+
+var filesToMove = paths.libs.concat(paths.html)										
+					.concat(paths.images)
+					.concat(paths.extras);
+					
+gulp.task('clean', function() {
+	return gulp.src(bases.dist)
+		.pipe(clean());
+});
+					
 gulp.task('browserify', function () {
-    gulp.src('src/js/main.js')
+    return gulp.src(paths.main, {cwd: bases.src})
         .pipe(browserify({
             transform: 'reactify',
             debug : true
         }))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(concat('main.js'))	
+		.pipe(uglify())		
+        .pipe(gulp.dest(bases.concat + 'js/'));
+});
+
+gulp.task('build-scripts', function() {
+	return gulp.src(paths.scripts, {cwd: bases.src})
+		.pipe(concat('support.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(bases.concat + 'js/'));
+});
+
+gulp.task('build-css', function() {
+  return gulp.src(paths.styles, {cwd: bases.src})
+    .pipe(minifyCss())
+    .pipe(gulp.dest(bases.concat + 'css/'));
+});
+
+gulp.task('copy-extras', function () {	
+	return gulp.src(filesToMove, { base: bases.src, cwd: bases.src })
+		.pipe(gulp.dest(bases.concat));
 });
 
 gulp.task('webkit-build', function () {
-
-    var nw = new NwBuilder({
+    var nw = new nwBuilder({
         version: '0.12.0',
-        files: ['./dist/**'],
-        platforms: ['win']
+        files: [ bases.dist + '**'],
+        platforms: ['win'],
+		buildDir: bases.webkit
     });
 
     nw.on('log', function (msg) {
@@ -33,37 +83,39 @@ gulp.task('webkit-build', function () {
     });
 });
 
-gulp.task('copy', function () {
-    gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('src/js/shell/*.*')
-        .pipe(gulp.dest('dist/js/shell'));
-
-    gulp.src('src/package.json')
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('src/css/*.*')
-        .pipe(gulp.dest('dist/css'));
-
-    gulp.src('src/js/lib/*.*')
-        .pipe(gulp.dest('dist/js/lib'));
-
-    gulp.src('src/assets/*.*')
-        .pipe(gulp.dest('dist/assets'));
-
-    gulp.src('src/fonts/*.*')
-        .pipe(gulp.dest('dist/fonts'));
-});
-
 gulp.task('test', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done);
+	return karma.start({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	}, function(code) {
+		if(code != 0) {
+			gutil.log(gutil.colors.red('ONE OR MORE TEST CASES HAVE FAILED! BUILD STOPPED!'));
+			process.exit(code);
+		}
+		else {
+			done();
+		}
+    });
 });
 
-gulp.task('default', ['test', 'browserify', 'copy']);
+gulp.task('default', function(callback) {
+	runSequence(['clean', 'test'],
+				['browserify', 'build-scripts', 'build-css', 'copy-extras'],				
+				callback);		
+});
+
+gulp.task('ci', ['browserify', 'copy-extras']);
+
+gulp.task('build', function(callback) {
+	runSequence(['clean', 'test'],
+				['browserify', 'build-scripts', 'build-css', 'copy-extras'],
+				'webkit-build',
+				callback);
+});
+
+// ===========================================================
+// TRIAL AND ERROR AREA 
+
 
 gulp.task('watch', function () {
     gulp.watch('src/**/*.*', ['default']);
@@ -83,6 +135,4 @@ gulp.task('serve', ['default'], function () {
     gulp.watch('src/**/*.*', ['default', browserSync.reload]);
 });
 
-gulp.task('ci', ['browserify', 'copy']);
-
-gulp.task('build', ['test', 'browserify', 'copy', 'webkit-build']);
+// ===========================================================
