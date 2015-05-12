3a7f6c528c33bcbb2118d16c62731d0d9c3211c4
zoneisuru <isuruj@zone24x7.com>
2015-05-12 23:15:14 +0530

    Code Cleanup on Radio buttons

--- src/js/components/form/app-RadioButton.js
+++ src/js/components/form/app-RadioButton.js
@@ -1,3 +1,27 @@
-/**
- * Created by IsuruJ on 5/12/2015.
- */
+/** @jsx React.DOM */
+var React = require('react');
+
+var RadioButton = React.createClass({
+    render : function(){
+
+        var keyUp= null;
+
+        if(this.props.keyUp){
+            keyUp = this.props.keyUp;
+        }
+
+        return (
+            <div className="radio radio-adv">
+                <label for="input-radio-1">
+                    <input className="access-hide" id="input-radio-1" name="input-radio" type="radio" onKeyUp={keyUp}>
+                        {this.props.label}
+                    </input>
+                    <span className="circle"></span>
+                    <span className="circle-check"></span>
+                </label>
+            </div>
+        );
+    }
+});
+
+module.exports = RadioButton;
