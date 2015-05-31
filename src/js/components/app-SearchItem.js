7cb9d8a9777b369023fa355be63d2ede5afd307a
yohangz <yohan.gz@hotmail.com>
2015-05-31 11:58:21 +0530

    Search Item JSLINT

--- src/js/components/app-SearchItem.js
+++ src/js/components/app-SearchItem.js
@@ -1,58 +1,55 @@
+/*global require, module, $*/
 /*Modification of http://tonyspiro.com/dev/react-typeahead-search*/
 /** @jsx React.DOM */
 var React = require('react');
 var AppActions = require('../actions/app-actions');
 
 var SearchItem = React.createClass({
-
     getInitialState: function () {
-      return {
-       "Classes": "tile result"
-      };
+        "use strict";
+        return {
+            "Classes": "tile result"
+        };
     },
-
-    _removeActive: function(){
-      this.setState( {
-       "Classes": "tile result"
-      });
+    _removeActive: function () {
+        "use strict";
+        this.setState({
+            "Classes": "tile result"
+        });
     },
-
-    _addActive: function(){
-      this.setState( {
-       "Classes": "result active"
-      });
+    _addActive: function () {
+        "use strict";
+        this.setState({
+            "Classes": "result active"
+        });
     },
-
-    _mouseOver: function () {   
-      this.props.clearCurrent();
-      this.setState( {
-          "Classes": "result active"
-      });
+    _mouseOver: function () {
+        "use strict";
+        this.props.clearCurrent();
+        this.setState({
+            "Classes": "result active"
+        });
     },
-
     _click: function () {
-      var id = $('#search-results .result.active').attr('data-id');
-      AppActions.addIssue(id);
-      this.props.togglePanel(false);
+        "use strict";
+        var id = $('#search-results .result.active').attr('data-id');
+        AppActions.addIssue(id);
+        this.props.togglePanel(false);
     },
-
     _mouseOut: function () {
-      this.setState( {
-          "Classes": "tile result"
-      });
+        "use strict";
+        this.setState({
+            "Classes": "tile result"
+        });
     },
-
     render : function () {
-      "use strict";
-      var item = this.props.item;
-      var id = this.props.item.id;
-      
-      return(
-              <div className="col-xs-9 col-md-9 section-box" className={this.state.Classes} id={"result-" + id} data-id={id} onMouseOver={this._mouseOver} onMouseOut={this._mouseOut} onClick={this._click}>
+        "use strict";
+        var item = this.props.item,
+            id = this.props.item.id;
+        return(<div className="col-xs-9 col-md-9 section-box" className={this.state.Classes} id={"result-" + id} data-id={id} onMouseOver={this._mouseOver} onMouseOut={this._mouseOut} onClick={this._click}>
                   <p className="list-item-title" dangerouslySetInnerHTML={{__html: item.project.name}}></p>
                   <p className="list-item-description" title={item.subject} dangerouslySetInnerHTML={{__html: item.formattedTitle}}></p>
-              </div>
-        );
+               </div>);
     }
 });
 
