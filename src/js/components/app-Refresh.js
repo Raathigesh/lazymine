04685450917ea1d136d2335ec3d2f61af9fc9ebb
yohangz <yohan.gz@hotmail.com>
2015-05-31 13:02:06 +0530

    Lint Fixes

--- src/js/components/app-Refresh.js
+++ src/js/components/app-Refresh.js
@@ -1,26 +1,25 @@
+/*global require, module*/
 /** @jsx React.DOM */
-var React = require('react');
-var Router = require('react-router');
-var AppActions = require('../actions/app-actions');
-var Link = Router.Link;
+var React = require('react'),
+    Router = require('react-router'),
+    AppActions = require('../actions/app-actions'),
+    Link = Router.Link;
 
 var Refresh = React.createClass({
-
-    _refreshTasks: function() {
+    _refreshTasks: function () {
+        "use strict";
         AppActions.refreshIssues();
     },
-
-    render : function(){
-        return (
-        <div className="fbtn-container top-refresh">
-            <div className="fbtn-inner" onClick={this._refreshTasks}>
-                <a className="fbtn fbtn-green fbtn-lg">
-                    <span className="fbtn-text">Refresh Items</span>
-                    <span className="fbtn-ori icon icon-refresh"></span>
-                </a>
-            </div>
-        </div>
-        );
+    render : function () {
+        "use strict";
+        return (<div className="fbtn-container top-refresh">
+                    <div className="fbtn-inner" onClick={this._refreshTasks}>
+                        <a className="fbtn fbtn-green fbtn-lg">
+                            <span className="fbtn-text">Refresh Items</span>
+                            <span className="fbtn-ori icon icon-refresh"></span>
+                        </a>
+                    </div>
+                </div>);
     }
 });
 
