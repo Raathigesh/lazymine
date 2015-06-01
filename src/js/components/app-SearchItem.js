362ed87c02ebb5b47a472a7a96aca46f3db9f8e3
Awad Maharoof <awadmaharoof@gmail.com>
2015-06-01 10:47:43 +0530

    Remove duplicate className attribute

--- src/js/components/app-SearchItem.js
+++ src/js/components/app-SearchItem.js
@@ -46,7 +46,7 @@ var SearchItem = React.createClass({
         "use strict";
         var item = this.props.item,
             id = this.props.item.id;
-        return(<div className="col-xs-9 col-md-9 section-box" className={this.state.Classes} id={"result-" + id} data-id={id} onMouseOver={this._mouseOver} onMouseOut={this._mouseOut} onClick={this._click}>
+        return(<div className={this.state.Classes} id={"result-" + id} data-id={id} onMouseOver={this._mouseOver} onMouseOut={this._mouseOut} onClick={this._click}>
                   <p className="list-item-title" dangerouslySetInnerHTML={{__html: item.project.name}}></p>
                   <p className="list-item-description" title={item.subject} dangerouslySetInnerHTML={{__html: item.formattedTitle}}></p>
                </div>);
