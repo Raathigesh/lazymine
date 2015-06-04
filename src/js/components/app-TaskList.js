7c67f5a87a9bf95f99ff46a849f07557c11f1e9c
Awad Maharoof <awadmaharoof@gmail.com>
2015-06-04 20:17:05 +0530

    Task list height is now set dynamically when application is resized

--- src/js/components/app-TaskList.js
+++ src/js/components/app-TaskList.js
@@ -1,38 +1,53 @@
 /*global require, module*/
 /** @jsx React.DOM */
 var React = require('react'),
-    Task = require('../components/app-Task'),
-    AppStore = require('../stores/app-base-store');
+Task = require('../components/app-Task'),
+AppStore = require('../stores/app-base-store');
 
 var TaskList = React.createClass({
     getInitialState: function () {
         "use strict";
+
+        var height = {
+          'height': document.documentElement.clientHeight - 140
+      };
+
         return {
             "items": null,
-            "containerHeight": 0
+            "componentHeight": height
         };
     },
-    render : function () {
-        "use strict";
-        var rows,
-            items = this.props.items,
-            activities = this.props.activities;
 
-        if (items) {
-            rows = items.map(function (item) {
-                return(<Task updatedTime="15" item={item} activities={activities} />);
-            });
-        }
+    handleResize: function(e) {
+        var height = {
+          'height': document.documentElement.clientHeight - 140
+      };
 
-    var containerHeight = {
-      height: document.documentElement.clientHeight - 138
-    };
-        return (
-            <div className="tile-wrap" style={containerHeight}>
-               {rows}
-            </div>
-        );
+      this.setState({componentHeight: height});
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  render : function () {
+    "use strict";
+    var rows,
+    items = this.props.items,
+    activities = this.props.activities;
+
+    if (items) {
+        rows = items.map(function (item) {
+            return(<Task updatedTime="15" item={item} activities={activities} />);
+        });
     }
+
+    return (
+        <div className="tile-wrap" style={this.state.componentHeight}>
+        {rows}
+        </div>
+    );
+  }
 });
 
 module.exports = TaskList;
