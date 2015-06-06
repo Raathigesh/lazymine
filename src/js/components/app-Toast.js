bf7c973770ce2637f783ab0263443b904c0ac909
yohangz <yohan.gz@hotmail.com>
2015-06-06 15:37:19 +0530

    Add dynamic identifier to toast

--- src/js/components/app-Toast.js
+++ src/js/components/app-Toast.js
@@ -1,15 +1,17 @@
 /*global require, module*/
 /** @jsx React.DOM */
-var React = require('react');
+var React = require('react'),
+    easyGid = require("easy-guid");
 
 var Toast = React.createClass({
     render : function () {
         "use strict";
-        var output = null;
+        var output = null,
+            identifier = easyGid.new();
 
         if (this.props.error !== null) {
             output =  <div className="toast toast-show">
-                <div className="tooltip bottom in" id="tooltip201389" style={{top: '-654px', left: '223.703125px', display: 'block', position: 'relative'}}>
+                <div className="tooltip bottom in" id={identifier} style={{top: '-654px', left: '223.703125px', display: 'block', position: 'relative'}}>
                     <div className="toast-inner tooltip-inner">
                         <a data-dismiss="toast">Dismiss</a>
                         <div className="toast-text">{ this.props.error.message }</div>
