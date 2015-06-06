da23039ad0503f3535a601867d00138d3c179bbb
yohangz <yohan.gz@hotmail.com>
2015-06-06 18:55:28 +0530

    JSLINT task list

--- src/js/components/app-TaskList.js
+++ src/js/components/app-TaskList.js
@@ -1,53 +1,51 @@
-/*global require, module*/
+/*global require, module, document, window*/
 /** @jsx React.DOM */
 var React = require('react'),
-Task = require('../components/app-Task'),
-AppStore = require('../stores/app-base-store');
+    Task = require('../components/app-Task'),
+    AppStore = require('../stores/app-base-store');
 
 var TaskList = React.createClass({
     getInitialState: function () {
         "use strict";
-
         var height = {
-          'height': document.documentElement.clientHeight - 160
-      };
+            'height': document.documentElement.clientHeight - 160
+        };
 
         return {
             "items": null,
             "componentHeight": height
         };
     },
-
-    handleResize: function(e) {
+    handleResize: function () {
+        "use strict";
         var height = {
-          'height': document.documentElement.clientHeight - 160
-      };
+            'height': document.documentElement.clientHeight - 160
+        };
 
-      this.setState({componentHeight: height});
-  },
+        this.setState({componentHeight: height});
+    },
+    componentDidMount: function () {
+        "use strict";
+        window.addEventListener('resize', this.handleResize);
+    },
+    render : function () {
+        "use strict";
+        var rows,
+            items = this.props.items,
+            activities = this.props.activities;
 
-  componentDidMount: function() {
-    window.addEventListener('resize', this.handleResize);
-  },
+        if (items) {
+            rows = items.map(function (item) {
+                return(<Task updatedTime="15" item={item} activities={activities} />);
+            });
+        }
 
-  render : function () {
-    "use strict";
-    var rows,
-    items = this.props.items,
-    activities = this.props.activities;
-
-    if (items) {
-        rows = items.map(function (item) {
-            return(<Task updatedTime="15" item={item} activities={activities} />);
-        });
+        return (
+            <div className="tile-wrap" style={this.state.componentHeight}>
+            {rows}
+            </div>
+        );
     }
-
-    return (
-        <div className="tile-wrap" style={this.state.componentHeight}>
-        {rows}
-        </div>
-    );
-  }
 });
 
 module.exports = TaskList;
