b7411ebe282b88eb2cf5151a4c047857d89584cc
zoneisuru <isuruj@zone24x7.com>
2015-06-07 17:14:02 +0530

    Fixed issue with app-Toast event lifecycle

--- src/js/components/app-Toast.js
+++ src/js/components/app-Toast.js
@@ -4,16 +4,34 @@ var React = require('react'),
     easyGid = require("easy-guid");
 
 var Toast = React.createClass({
-    render : function () {
+    getInitialState: function () {
+        return {
+            display: false
+        };
+    },
+    componentWillReceiveProps: function (nextProps) {
+        if (nextProps.error !== null){
+            this.setState({
+                display: true
+            });
+        }
+    },
+    _closeToast: function() {
+        this.setState({
+            display: false
+        });
+    },
+    render: function () {
         "use strict";
         var output = null,
             identifier = easyGid.new();
 
-        if (this.props.error !== null) {
-            output =  <div className="toast toast-show">
-                <div className="tooltip bottom in" id={identifier} style={{top: '-654px', left: '223.703125px', display: 'block', position: 'relative'}}>
+        if (this.state.display) {
+            output = <div className="toast toast-show">
+                <div className="tooltip bottom in" id={identifier}
+                     style={{top: '-654px', left: '223.703125px', display: 'block', position: 'relative'}}>
                     <div className="toast-inner tooltip-inner">
-                        <a data-dismiss="toast">Dismiss</a>
+                        <a href="javascript:void(0)" className="pull-right" onClick={this._closeToast}>Dismiss</a>
                         <div className="toast-text">{ this.props.error.message }</div>
                     </div>
                 </div>
@@ -21,7 +39,7 @@ var Toast = React.createClass({
         }
 
         return (
-           output
+            output
         );
     }
 });
