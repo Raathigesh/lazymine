d11619cdec92ae1607d29ffc2e09b56548a60aab
Awad Maharoof <awadmaharoof@gmail.com>
2015-08-07 10:13:55 +0530

    Dummy data with styling for weekly time summary card - WIP

--- src/js/components/app-WeekTimeCard.js
+++ src/js/components/app-WeekTimeCard.js
@@ -7,12 +7,42 @@ var WeekTimeCard = React.createClass({
     render: function () {
         "use strict";
         return (
-            <div className="card">
+            <div className="card card-blue">
                 <div className="card-main">
-                    <div className="card-inner">
-                        <p className="card-heading">Weekly Summary</p>
+                    <div className="card-header">
+                        <div className="card-inner">
+                            <p className="card-heading">Weekly Summary</p>
+                        </div>
                     </div>
-                    <div className="card-action"> 
+                    <div className="card-img">
+                        <span className="card-key-value">38</span> <span className="card-sub-heading">Hours entered for the week August 2 - August 8</span>
+                    </div>
+                    <div className="card-inner">
+                        <div className="daily-time-total">
+                            <span className="text-left">
+                                Sunday August 2
+                            </span>
+                            <span className="pull-right">
+                                7.5h
+                            </span>
+                        </div>
+                        <div className="tile tile-collapse">
+                            <div className="card-action">
+                                <div className="pull-left tile-side">
+                                    <div className="avatar avatar-sm avatar-multi">
+                                        <span className="">R</span> 
+                                    </div>
+                                </div>
+                                <div className="tile-action tile-action-show">
+                                    1.5h
+                                </div>
+                                <div class="tile-inner">
+    <div class="text-overflow">
+        OAC-254 OAC Tiger Beta Release : Submit and c
+    </div>
+</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
