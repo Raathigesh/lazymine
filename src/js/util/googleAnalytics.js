67afd3ad983cf1be0ea46aa4f343f148ac4073a6
Chathuranga <chathurangaw@zone24x7.com>
2015-10-04 20:18:20 +0530

    Making the Google Analytics integration bit Unit Testing friendly.

--- src/js/util/googleAnalytics.js
+++ src/js/util/googleAnalytics.js
@@ -1,13 +1,17 @@
 var GoogleAnalytics = function(){
     "user strict";
-    ga_storage._setAccount('UA-65910418-1');
-    ga_storage._setDomain('none');
+    if(typeof ga_storage !== 'undefined'){
+        ga_storage._setAccount('UA-65910418-1');
+        ga_storage._setDomain('none');
+    }
 };
 
 GoogleAnalytics.prototype = (function(){
     "use strict";
     var trackPageView = function(relativeURL){
-        ga_storage._trackPageview(relativeURL);
+        if(typeof ga_storage !== 'undefined'){
+            ga_storage._trackPageview(relativeURL);
+        }
     };
     return {
         trackPageView: trackPageView
